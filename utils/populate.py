e96f6e7c7dd16f6c1f3a0dab9cc35f1a239bca22
Alex Egg <eggie5@gmail.com>
2017-02-24 13:48:37 -0800

    added image_url loading

--- utils/populate.py
+++ utils/populate.py
@@ -1,3 +1,34 @@
+#first load image LUT
+def parse(path):
+  g = open(path, 'r')
+  for l in g:
+    yield eval(l)
+    
+import json
+
+
+#input is path to metadata file
+def build_image_lut(path):
+  images={}
+
+  i=0
+  j=0
+  for l in parse(path):
+    try:
+      i+=1
+      image_url = l['imUrl']
+      asin = l['asin']
+      images[asin]=image_url
+    except KeyError:
+      j+=1
+  return images
+    
+# takes about 16 min to go through 10G of data
+print "starting LUT build..."
+path ="/Users/alexegg/Development/dse/capstone/data/metadata.json"
+image_lut=build_image_lut(path)
+print "Finished LUT build..."
+
 #read clothing data from simple_out csv or some file and load into PG
 import csv
 import psycopg2
@@ -12,13 +43,14 @@ with open(path, 'rb') as reviews_file:
   reader = csv.reader(reviews_file, delimiter=' ')
   for row in reader:
     asin = row[1]
+    image_url = image_lut[asin]
     if asin in items:
       continue
     else:
       items[asin]=True
       
     query =  "INSERT INTO items (asin, image_url) VALUES (%s, %s);"
-    data = (asin, "...")
+    data = (asin, image_url)
     cursor.execute(query, data)
     
     
