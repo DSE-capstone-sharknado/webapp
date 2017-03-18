1144067feb45f00aaf267f705bb199c2f273af1d
Alex Egg <eggie5@gmail.com>
2017-03-18 10:53:49 -0700

    updated ports

--- web/deploy/deploy.sh
+++ web/deploy/deploy.sh
@@ -5,7 +5,7 @@ docker-compose build
 docker tag webapp_web gcr.io/com-eggie5-blog/sharknado-web
 gcloud docker push gcr.io/com-eggie5-blog/sharknado-web
 kubectl apply -f k8s/ --namespace=$NS
-kubectl create secret generic db-secret --from-literal=username=postgres --from-literal=password= --namespace $NS
+# kubectl create secret generic db-secret --from-literal=username=postgres --from-literal=password= --namespace $NS
 
 
 # gcloud compute disks create sharknado-db-disk --type pd-standard  --zone us-west1-a
